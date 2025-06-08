6fef083f9dc977711e98c5dd3279c0439e1725de
Prateek Rana <prateekrana@Prateek-Rana.local>
2025-06-08 15:10:47 +0530

    hero section

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Hero from "./components/Hero";
+import About from "./components/About";
+
+function App() {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero/>
+      <About/>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div id='about' className='min-h-screen w-screen'>
+        <div className='relative mb-8 mt-36 flex flex-col items-center gap-5'>
+            <h2 className='font-general text-sm uppercase md:text-[10px]'>WELCOME TO ZENTRY</h2>
+            <div className='mt-5 text-center font-bold uppercase text-4xl md:text-6rem'>
+                Discover the world's <br /> largest shared adventure
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
